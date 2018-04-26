8a27e1d3e68c8e8a95c6cf7f6221b522bd5965ea
Hermans Dirk (100) <dirk.hermans@cm.be>
2018-04-26 15:21:54 +0200

    ItemOverviewView grid needs debug

--- ordergui-webapp/src/main/java/com/switchfully/vaadin/ordergui/webapp/views/items/ItemOverviewView.java
+++ ordergui-webapp/src/main/java/com/switchfully/vaadin/ordergui/webapp/views/items/ItemOverviewView.java
@@ -1,33 +1,89 @@
 package com.switchfully.vaadin.ordergui.webapp.views.items;
 
+import com.switchfully.vaadin.ordergui.interfaces.items.Item;
 import com.switchfully.vaadin.ordergui.interfaces.items.ItemResource;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
-import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
+
+import java.util.List;
 
 public class ItemOverviewView extends CustomComponent implements View {
 
     private final VerticalLayout mainLayout;
+    private final HorizontalLayout headerLayout;
+    private final HorizontalLayout gridLayout;
+
+    private final Label items;
+    private final TextField itemFilter;
+    private final Button btnFilter;
+    private final Button btnNewItem;
+
+    private final Grid grid;
 
     public ItemOverviewView(ItemResource itemResource) {
-        this.mainLayout = new VerticalLayout();
+        this.items = createItemLabel();
+        this.itemFilter = createItemFilter();
+        this.btnFilter = createButtonFilter();
+        this.btnNewItem = createButtonNewItem();
 
-        itemResource.getItems()
-                .forEach(item ->
-                        mainLayout.addComponent(
-                                new HorizontalLayout(
-                                        new Label("--> " + item.name + " €" + item.price))));
+        this.grid = createGrid(itemResource);
 
-        mainLayout.addComponent(
-                new HorizontalLayout(
-                        new Label("ITEMS:")
-                )
-        );
+        this.headerLayout = new HorizontalLayout(items, itemFilter, btnFilter, btnNewItem);
+        headerLayout.setSpacing(true);
+        this.gridLayout = new HorizontalLayout(grid);
+        this.mainLayout = new VerticalLayout(headerLayout, gridLayout);
+        mainLayout.setSpacing(true);
+
+//        itemResource.getItems()
+//                .forEach(item ->
+//                        mainLayout.addComponent(
+//                                new HorizontalLayout(
+//                                        new Label("--> " + item.name + " €" + item.price))));
+
+//        mainLayout.addComponent(
+//                new HorizontalLayout(
+//                        new Label("ITEMS:")
+//                )
+//        );
 
         setCompositionRoot(mainLayout);
+    }
+
+    private Grid createGrid(ItemResource itemResource) {
+        Grid grid = new Grid();
+
+        List<Item> itemList = itemResource.getItems();
+
+        BeanItemContainer<Item> container =
+                new BeanItemContainer<>(Item.class, itemList);
+
+        grid.setColumns("name", "description");
+
+        grid.setContainerDataSource(container);
+
+        return grid;
+    }
+
+    private Button createButtonNewItem() {
+        Button btn = new Button("New Item");
+        return btn;
+    }
+
+    private Button createButtonFilter() {
+        Button btn = new Button("Filter");
+        return btn;
+    }
+
+    private TextField createItemFilter() {
+        TextField textField = new TextField();
+        textField.setInputPrompt("Filter by name");
+        return textField;
+    }
+
+    private Label createItemLabel() {
+        return new Label("Items");
     }
 
     @Override
